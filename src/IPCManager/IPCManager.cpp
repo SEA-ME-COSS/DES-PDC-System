37465c04df983159ae015722dc7411eba407d289
dongdongO <tjehdghks4356@gmail.com>
2023-11-16 13:06:35 +0100

    #1 Delete IPCManger rebooting part for yocto supervisor

--- src/IPCManager/IPCManager.cpp
+++ src/IPCManager/IPCManager.cpp
@@ -22,35 +22,6 @@ int main()
 
     while (1)
     {
-        if (!sender.IVICompositorTargetProxy->isAvailable()) {
-            exitCode = system("sh ../reboot_script/reboot_ivi_compositor.sh");
-            // Check if IVICompositorTargetProxy is not available and execute a reboot script
-        }
-        if (!sender.CANSenderTargetProxy->isAvailable()) {
-            exitCode = system("sh ../reboot_script/reboot_can_sender.sh");
-            // Check if CANSenderTargetProxy is not available and execute a reboot script
-        }
-        if (!sender.PiracerSenderTargetProxy->isAvailable()) {
-            exitCode = system("sh ../reboot_script/reboot_piracer_sender.sh");
-            // Check if PiracerSenderTargetProxy is not available and execute a reboot script
-        }
-        if (!sender.PiracerControllerTargetProxy->isAvailable()) {
-            exitCode = system("sh ../reboot_script/reboot_piracer_controller.sh");
-            // Check if PiracerControllerTargetProxy is not available and execute a reboot script
-        }
-        if (!sender.InstrumentClusterTargetProxy->isAvailable()) {
-            exitCode = system("sh ../reboot_script/reboot_instrument_cluster.sh");
-            // Check if InstrumentClusterTargetProxy is not available and execute a reboot script
-        }
-        if (!sender.HeadUnitTargetProxy->isAvailable()) {
-            exitCode = system("sh ../reboot_script/reboot_head_unit.sh");
-            // Check if HeadUnitTargetProxy is not available and execute a reboot script
-        }
-        if (!sender.PDCUnitTargetProxy->isAvailable()) {
-            exitCode = system("sh ../reboot_script/reboot_pdc_unit.sh");
-            // Check if PDCUnitTargetProxy is not available and execute a reboot script
-        }
-
         usleep(1000000); // Sleep for one second
     }
 
